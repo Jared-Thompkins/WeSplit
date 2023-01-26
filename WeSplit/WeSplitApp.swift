d2fe399e18a9cce622fe11c7a49e1fbc5c4c4a08
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-01-26 01:15:18 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jared Thompkins on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
